3b19bb4b0e288f1018abc4d84826d752d1e8557a
meysam razmi <m.razmi.92@gmail.com>
2020-04-22 15:30:27 +0430

    add colleague companies sub menu

---
+++ sites/all/themes/nutland/templates/menu-link.vars.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @file
+ * Stub file for "menu_link" theme hook [pre]process functions.
+ */
+
+/**
+ * Pre-processes variables for the "menu_link" theme hook.
+ *
+ * See theme function for list of available variables.
+ *
+ * @param array $variables
+ *   An associative array of variables, passed by reference.
+ *
+ * @see bootstrap_menu_link()
+ * @see theme_menu_link()
+ *
+ * @ingroup theme_preprocess
+ */
+function nutland_preprocess_menu_link(array &$variables) {
+  $element = &$variables['element'];
+  if($element['#original_link']['mlid'] == 734){
+    $block = block_load('block', '8');
+    $output = _block_get_renderable_array(_block_render_blocks(array($block)));
+    $element['#below'] = array($output);
+    $element['#original_link']['has_children'] = 1;
+  }
+}
