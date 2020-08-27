6489712e3018eb1829570067c4fb247a6cbd04c2
meysam razmi <m.razmi.92@gmail.com>
2020-08-27 16:50:27 +0430

    add new display style for headers. max character for summery

--- sites/all/themes/adminimal_theme/js/razmi-costum.js
+++ sites/all/themes/adminimal_theme/js/razmi-costum.js
@@ -1,24 +1,34 @@
 var $ = jQuery;
 $(document).ready(function () {
 
-$('.form-textarea-wrapper .grippie').click( function () {
-  $('.form-textarea-wrapper textarea').addClass('dir-ltr');
-  $('.form-textarea-wrapper textarea').css('direction', 'ltr');
+  $('.form-textarea-wrapper .grippie').click( function () {
+    $('.form-textarea-wrapper textarea').addClass('dir-ltr');
+    $('.form-textarea-wrapper textarea').css('direction', 'ltr');
 
-});
-$('#field-classes-timing-values > tbody > tr .field-vip input').change(function(){
-	if($(this).prop("checked")){
-		$(this).parents('tr').addClass('groupClass')
-	}else{
-		$(this).parents('.groupClass').removeClass('groupClass')
-	}
-})
-$('#field-classes-timing-values > tbody > tr').each(function(){
-	if($(this).find('.field-vip input').prop("checked")){
-		$(this).addClass('groupClass')
-	}
-})
+  });
+  $('#field-classes-timing-values > tbody > tr .field-vip input').change(function(){
+    if($(this).prop("checked")){
+      $(this).parents('tr').addClass('groupClass')
+    }else{
+      $(this).parents('.groupClass').removeClass('groupClass')
+    }
+  })
+  $('#field-classes-timing-values > tbody > tr').each(function(){
+    if($(this).find('.field-vip input').prop("checked")){
+      $(this).addClass('groupClass')
+    }
+  })
 
-
+  $('textarea.text-summary').keypress(function(e) {
+    var tval = $(this).val(),
+      tlength = tval.length,
+      set = 500,
+      remain = parseInt(set - tlength);
+    $(this).parent().parent().find('.description').text('برای استفاده از مقدار برش‌یافته متن کامل به عنوان خلاصه، این را خالی بگذارید. '+ remain +'/'+ set +' کاراکتر باقی مانده.');
+    if (remain <= 0 && e.which !== 0 && e.charCode !== 0) {
+      $(this).val((tval).substring(0, set));
+      return false;
+    }
+  })
 });
 
