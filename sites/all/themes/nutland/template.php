708872232b2f748c96fdcbd6cff43a2ae2b864c8
Meysam Razmi <m.razmi.92@gmail.com>
2022-04-16 15:12:09 +0430

    add link for subset companies

--- sites/all/themes/nutland/template.php
+++ sites/all/themes/nutland/template.php
@@ -12,3 +12,23 @@
 function nutland_theme_registry_alter(&$theme_registry) {
   $theme_registry['status_messages']['function'] = 'theme_pines_notify';
 }
+
+function nutland_ds_pre_render_alter(&$layout_render_array, $context, $vars) {
+  if ($context['entity']->type == 'subset') {
+    foreach ($layout_render_array['footer'] as &$field) {
+      if (!empty($field['#field_name']) && $field['#field_name'] == 'field_company') {
+
+        $index = 0;
+        foreach ($field as $key => &$item) {
+          if (is_numeric($key) && !empty($context['entity']->field_company_links['und'][$index]['url'])) {
+            $item['#path'] = [
+              'path' => $context['entity']->field_company_links['und'][$index]['url'],
+            ];
+            $index++;
+          }
+        }
+
+      }
+    }
+  }
+}
